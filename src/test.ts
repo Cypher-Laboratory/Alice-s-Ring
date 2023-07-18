186ee2e0bafad57b1e7e7814cf22ffeca2ce158b
Hussenet Thomas <Lejamon.c@protonmail.com>
2023-07-18 18:40:10 +0200

    new branch

--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,9 @@
 import { altSchnorrSignature } from "./shnorrSignature/alt_Schnorr";
 import { keccak256 } from "js-sha3"; // fonction de hashage reconnue ?
 import { G, randomBigint, getRandomSecuredNumber } from "./utils";
-import { ringSignature, ringSignatureVerify } from "./ring_signature";
-
+//import { ringSignature, ringSignatureVerify } from "./ring_signature";
+import {ringSignature} from "./ring_signator";
+import{sigVerif} from "./ring_verifier";
 const privKey =
   randomBigint(
     0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141n,
@@ -29,12 +30,14 @@ for (let i = 0; i < 9; i++) {
   ]);
 }
 
-const sig = ringSignature("Hello World !", ring, privKey);
+const sig = ringSignature("1000", ring, privKey);
 
-const signerPubkey: [bigint, bigint] = [privKey * G[0], privKey * G[1]];
-const pubkeys = [signerPubkey].concat(ring);
+console.log(sig);
+
+/*const signerPubkey: [bigint, bigint] = [privKey * G[0], privKey * G[1]];
+const pubkeys = [signerPubkey].concat(ring);*/
 
 console.log(
   "verify: ",
-  ringSignatureVerify("Hello World !", pubkeys, sig as [bigint, bigint[]]),
+  sigVerif("1000",sig?.cvalue ?? "sksksks",sig?.responses ?? [], sig?.ring ?? [[0n,0n]]),
 );
