ddc463143b20e57a9f3578f15f69b45cae2cc24c
Nathan HERVIER <113121626+Elli610@users.noreply.github.com>
2023-07-18 21:36:58 +0200

    fix utils export

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,3 @@
 export { P, G, Gx, Gy, l } from "./curveConst";
 export { randomBigint, getRandomSecuredNumber } from "./randomNumbers";
-export { compressPoint } from "./pointCompression";
 export { modulo } from "./modulo";
