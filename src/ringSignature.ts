13f767a4bf2e1fe0f11f7b0c8e5c48d68fa077fd
Nathan HERVIER <113121626+Elli610@users.noreply.github.com>
2023-07-18 22:18:58 +0200

    change cees type to bigint[]

--- src/ringSignature.ts
+++ src/ringSignature.ts
@@ -1,16 +1,37 @@
 import { keccak256 } from "js-sha3";
 import { G, P, modulo, randomBigint } from "./utils";
 
-interface RingSignature {
-  cees: string[];
+interface IRingSignature {
+  cees: bigint[];
   responses: bigint[];
 }
+
+
+export class RingSignature {
+  cees: bigint[];
+  responses: bigint[];
+  constructor(cees: bigint[], responses: bigint[]) {
+    this.cees = cees;
+    this.responses = responses;
+  }
+
+  static fromIRingSignature(sig: IRingSignature): RingSignature {
+    return new RingSignature(
+      sig.cees.map((x) => BigInt("0x" + x)),
+      sig.responses,
+    );
+  }
+
+
+}
+
+
 
 export function sign(
   witnesses: [[bigint, bigint]],
   signerPk: bigint,
   message: string,
-): RingSignature {
+): IRingSignature {
   // generate random number alpha
   const alpha = randomBigint(P);
 
@@ -23,61 +44,69 @@ export function sign(
   }
 
   // contains all the c from pi+1 to n
-  const cValuesPI1N: string[] = [];
+  const cValuesPI1N: bigint[] = [];
   // calcul of C pi+1
   cValuesPI1N.push(
-    keccak256(
-      witnesses + message + String(alpha * G[0]) + String(alpha * G[1]),
-    ),
+    BigInt("0x" +
+      keccak256(
+        witnesses + message + String(alpha * G[0]) + String(alpha * G[1]),
+      ),
+    )
   );
 
   for (let i = pi + 2; i < witnesses.length; i++) {
     cValuesPI1N.push(
+      BigInt("0x" +
+        keccak256(
+          witnesses +
+            message +
+            String(
+              fakeResponses[i] * G[0] +
+                BigInt("0x" + cValuesPI1N[i - pi - 2]) * witnesses[i][0] +
+                BigInt("0x" + cValuesPI1N[i - pi - 2]) * witnesses[i][1] +
+                fakeResponses[i] * G[1],
+            ),
+        ),
+      )
+    );
+  }
+  //contains all the c from 0 to pi-1
+  const cValues0PI1: bigint[] = [];
+
+  cValues0PI1.push(
+    BigInt("0x" +
+      keccak256(
+        witnesses +
+          message +
+          String(
+            fakeResponses[witnesses.length - 1] * G[0] +
+              BigInt("0x" + cValuesPI1N[cValuesPI1N.length - 1]) *
+                witnesses[witnesses.length - 1][0] +
+              BigInt("0x" + cValuesPI1N[cValuesPI1N.length - 1]) *
+                witnesses[witnesses.length - 1][1] +
+              fakeResponses[witnesses.length - 1] * G[1],
+          ),
+      )
+    ),
+  );
+
+  for (let i = 1; i < pi + 1; i++) {
+    cValues0PI1[i] = BigInt("0x" + 
       keccak256(
         witnesses +
           message +
           String(
             fakeResponses[i] * G[0] +
-              BigInt("0x" + cValuesPI1N[i - pi - 2]) * witnesses[i][0] +
-              BigInt("0x" + cValuesPI1N[i - pi - 2]) * witnesses[i][1] +
+              BigInt("0x" + cValues0PI1[i - 1]) * witnesses[i][0] +
+              BigInt("0x" + cValues0PI1[i - 1]) * witnesses[i][1] +
               fakeResponses[i] * G[1],
           ),
-      ),
-    );
-  }
-  //contains all the c from 0 to pi-1
-  const cValues0PI1: string[] = [];
-
-  cValues0PI1.push(
-    keccak256(
-      witnesses +
-        message +
-        String(
-          fakeResponses[witnesses.length - 1] * G[0] +
-            BigInt("0x" + cValuesPI1N[cValuesPI1N.length - 1]) *
-              witnesses[witnesses.length - 1][0] +
-            BigInt("0x" + cValuesPI1N[cValuesPI1N.length - 1]) *
-              witnesses[witnesses.length - 1][1] +
-            fakeResponses[witnesses.length - 1] * G[1],
-        ),
-    ),
-  );
-
-  for (let i = 1; i < pi + 1; i++) {
-    cValues0PI1[i] = keccak256(
-      witnesses +
-        message +
-        String(
-          fakeResponses[i] * G[0] +
-            BigInt("0x" + cValues0PI1[i - 1]) * witnesses[i][0] +
-            BigInt("0x" + cValues0PI1[i - 1]) * witnesses[i][1] +
-            fakeResponses[i] * G[1],
-        ),
+      )
     );
   }
 
   //concatenate CVAlues0PI1 and CVAluesPI1N
-  const cValues: string[] = cValues0PI1.concat(cValuesPI1N);
+  const cValues: bigint[] = cValues0PI1.concat(cValuesPI1N);
 
   //define the signer response
   const signerResponse = modulo(
@@ -95,7 +124,7 @@ export function sign(
 }
 
 export function verify(
-  sig: RingSignature,
+  sig: IRingSignature,
   witnesses: [[bigint, bigint]],
   message: string,
 ) {
@@ -116,18 +145,21 @@ export function verify(
     );
   }
 
-  const firstValue: string = keccak256(
-    witnesses +
-      message +
-      String(
-        sig.responses[sig.responses.length - 1] * G[0] +
-          BigInt("0x" + sig.cees[sig.cees.length - 1]) *
-            witnesses[witnesses.length - 1][0] +
-          BigInt("0x" + sig.cees[sig.cees.length - 1]) *
-            witnesses[witnesses.length - 1][1] +
-          sig.responses[sig.responses.length - 1] * G[1],
-      ),
+  const firstValue: bigint = 
+  BigInt("0x" +
+    keccak256(
+      witnesses +
+        message +
+        String(
+          sig.responses[sig.responses.length - 1] * G[0] +
+            BigInt("0x" + sig.cees[sig.cees.length - 1]) *
+              witnesses[witnesses.length - 1][0] +
+            BigInt("0x" + sig.cees[sig.cees.length - 1]) *
+              witnesses[witnesses.length - 1][1] +
+            sig.responses[sig.responses.length - 1] * G[1],
+        ),
+    ),
   );
 
-  return [firstValue].concat(values)[0] === sig.cees[0];
+  return firstValue === sig.cees[0];
 }
