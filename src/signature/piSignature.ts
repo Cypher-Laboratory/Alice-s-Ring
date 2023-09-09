2a04035452c16f478e6bd796035529946bed0173
Nathan <113121626+Elli610@users.noreply.github.com>
2023-09-09 12:58:24 +0200

    add license

--- src/signature/piSignature.ts
+++ src/signature/piSignature.ts
@@ -1,9 +1,15 @@
+/*
+  This TypeScript library is the exclusive property of Cypher Lab (https://www.cypherlab.fr/) 
+  and is exclusively reserved for the use of gemWallet. Any form of commercial use, including but 
+  not limited to selling, licensing, or generating revenue from this code, is strictly prohibited.
+*/
+
 import { modulo } from "../utils";
 
 /**
  * Compute the signature from the actual signer
  *
- * @param alpha - the alpha value (random number generated at the beginning of the signature)
+ * @param alpha - the alpha value
  * @param c - the c[pi] value
  * @param signerPrivKey - the private key of the signer
  * @param p - the order of the curve
