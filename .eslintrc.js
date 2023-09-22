44d536d71942936d3946edca92ede20cd4848494
Nathan <113121626+Elli610@users.noreply.github.com>
2023-09-22 22:38:12 +0200

    new build

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   plugins: ["@typescript-eslint"],
   rules: {
-    indent: ["error", 2, { SwitchCase: 2 }],
+    indent: ["error", 2, { SwitchCase: 1 }],
     semi: ["error", "always"],
     "max-len": ["error", 200],
     camelcase: "off",
