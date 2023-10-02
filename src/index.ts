befe68a01f8760734e4cbef0e3e7955a38b30a90
Nathan <113121626+Elli610@users.noreply.github.com>
2023-10-02 17:31:37 +0200

    export CurveName

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 export { piSignature } from "./signature/piSignature";
 
-export { Curve, Point, randomBigint, getRandomSecuredNumber } from "./utils";
+export {
+  Curve,
+  CurveName,
+  Point,
+  randomBigint,
+  getRandomSecuredNumber,
+} from "./utils";
 
 export { RingSignature, PartialSignature } from "./ringSignature";
