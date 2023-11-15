9ddd1fd91c96f16d887fe2a3211d743cd8f37685
Hussenet Thomas <Lejamon.c@protonmail.com>
2023-11-15 18:31:14 +0100

    run CI

--- test/point/fromString.spec.ts
+++ test/point/fromString.spec.ts
@@ -1,4 +1,4 @@
-import { Curve, CurveName, Point } from "../../src";
+import { Point } from "../../src";
 import {
   valid_string_point_ed25519,
   valid_string_point_secp256k1,
@@ -6,7 +6,6 @@ import {
   invalid_string_point_secp256k1,
 } from "../data";
 describe("Point class fromString()", () => {
-  
   it("should return a point from a string representation of the point, ed25519", () => {
     const point = Point.fromString(valid_string_point_ed25519);
     expect(point).toBeInstanceOf(Point);
@@ -27,5 +26,5 @@ describe("Point class fromString()", () => {
 
   it("should throw an error if the string is void", () => {
     expect(() => Point.fromString("")).toThrow();
-    }); 
+  });
 });
