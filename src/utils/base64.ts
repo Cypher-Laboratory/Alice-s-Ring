ff48d502ab635c290c4a98fa8ef27e311ac63fa6
Hussenet Thomas <Lejamon.c@protonmail.com>
2024-02-19 10:10:06 +0100

    rename the foler formatData to serializeData, and solve 82 by creaing a file base64.ts

---
+++ src/utils/base64.ts
@@ -0,0 +1,3 @@
+export const base64Regex =
+  // eslint-disable-next-line no-useless-escape
+  /^(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)?$/;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,5 @@
 export { randomBigint, getRandomNumber } from "./randomNumbers";
 export { modulo } from "./modulo";
-export { serializeRing } from "./formatData/serializeRing";
+export { serializeRing } from "./serializeData/serializeRing";
 export { keccak_256, hash, sha_512 } from "./hashFunction";
-export const base64Regex =
-  // eslint-disable-next-line no-useless-escape
-  /^(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)?$/;
+export { base64Regex } from "./base64";
